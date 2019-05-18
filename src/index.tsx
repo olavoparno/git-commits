69f6d5bef6a0b06c965b8a18b89ea6312c170a61
Olavo Parno <olavoparno@gmail.com>
2019-05-18 20:43:06 -0300

    feat(input): add debounce input dep

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider, Subscribe } from 'unstated'
+import { DebounceInput } from 'react-debounce-input';
 
 import AppContainer from 'components/container'
 import "./styles.scss";
 
 function App(): JSX.Element {
+  const handleChange = (e: any, container: AppContainer) => {
+    const newRepo = e.target.value
+    if (newRepo && newRepo.length) container.fetchCommits(newRepo)
+  }
   return (
     <Provider>
       <Subscribe to={[AppContainer]}>
       {
         (container: AppContainer) => {
           return !container.state.isLoading ? (
+            container.state.validRepo ? (
             <div className="app-container">
-              {console.log('State', container.state.commits)}
-              sasa
+            {console.log('State', container.state)}
+            <DebounceInput
+              minLength={2}
+              debounceTimeout={800}
+              placeholder={container.state.currentRepo}
+              className="repo-input"
+              onChange={event => handleChange(event, container)} />
             </div>
+            ) : (
+              <h1>Invalid Repo</h1>
+            )
           ) :
           (
             <h1>Loading...</h1>
